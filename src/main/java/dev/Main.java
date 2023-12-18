5fdaf241773d400e900f95e876cc550869ee20fe
Taiaba-Rahat-Ritu <taiabarahatritu@gmail.com>
2023-12-18 17:25:26 +0600

    final lab task 1

--- src/main/java/dev/Main.java
+++ src/main/java/dev/Main.java
@@ -1,4 +1,5 @@
 package dev;
-
 public class Main {
+
 }
+
--- src/main/java/dev/domain/Student.java
+++ src/main/java/dev/domain/Student.java
@@ -1,61 +1,68 @@
 package dev.domain;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 public class Student {
+    @NotNull
+    private int id;
 
     @NotNull
-    private Integer id;
-
-    @NotNull
-    @Size(max = 100)
     private String name;
 
     @NotNull
-    @Email
-    @Size(max = 100)
+    @Pattern(regexp = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$", message = "Incorrect email format")
     private String email;
 
     @NotNull
+    @Size(min = 8)
+    private String password;
+
+    @NotNull
     @Past
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @IAgeValidatorInterface
     private LocalDate dateOfBirth;
 
+    @NotBlank
     @NotNull
-    private Gender gender;
+    private String gender;
 
-    @NotNull
+    @NotBlank
     private String quota = "N/A";
 
-    @NotNull
+    @NotBlank
     private String country = "Bangladeshi";
 
     public Student() {
     }
 
-    public Student(Integer id, String name, String email, LocalDate dateOfBirth, Gender gender, String quota, String country) {
+    public Student(int id, String name, String email, String password, LocalDate dateOfBirth, String gender, String quota, String country) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.password = password;
         this.dateOfBirth = dateOfBirth;
         this.gender = gender;
         this.quota = quota;
         this.country = country;
     }
 
-    public Integer getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(int id) {
         this.id = id;
     }
 
-    public String getName() {
+    public String getname() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setname(String name) {
         this.name = name;
     }
 
@@ -67,6 +74,14 @@ public class Student {
         this.email = email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
@@ -75,11 +90,11 @@ public class Student {
         this.dateOfBirth = dateOfBirth;
     }
 
-    public Gender getGender() {
+    public String getGender() {
         return gender;
     }
 
-    public void setGender(Gender gender) {
+    public void setGender(String gender) {
         this.gender = gender;
     }
 
@@ -98,4 +113,5 @@ public class Student {
     public void setCountry(String country) {
         this.country = country;
     }
+
 }
